43836c2717a1f67abb45945cf9cef57b1827e594
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2022-04-02 15:31:31 +0530

    passed data to page

---
+++ blog-front/src/Components/Page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Page = (props) => {
+  const { post } = props;
+  console.log(post);
+  return <h1>Page</h1>;
+};
+
+export default Page;
--- blog-front/src/View/SinglePost.jsx
+++ blog-front/src/View/SinglePost.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { UserContext } from "../App";
+import Page from "../Components/Page";
 
 const SinglePost = () => {
   const { postName } = useParams();
-  console.log(postName);
+  const { data } = useContext(UserContext);
 
-  return <h1>=ji</h1>;
+  const [post, setPost] = useState("");
+
+  useEffect(() => {
+    if (data?.length > 0) {
+      let auxData = data?.filter((item) => item.postTitle.trim() === postName);
+      setPost(auxData[0]);
+    }
+  }, [data]);
+
+  return <Page post={post} />;
 };
 
 export default SinglePost;
